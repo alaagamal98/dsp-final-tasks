e6723f078673d58ce07ed4a3c71a0c9d812ab93e
alaagamal98 <alaagamal.98@gmail.com>
2020-05-15 00:20:19 +0200

    problem1

--- Problem1/main.py
+++ Problem1/main.py
@@ -18,9 +18,9 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.MapsButton.clicked.connect(self.MapsGraph)
         self.ui.SortedButton.clicked.connect(self.SortedGraph)
         self.ui.SecondBubbleButton.clicked.connect(self.SecondBubbleGraph)
-        self.data = pd.read_excel('Database/PreProcessedCOVID-192.xlsx', sheet_name='Sheet1')
+        self.data = pd.read_excel('Database/PreProcessedCOVID-19.xlsx', sheet_name='Sheet1')
         self.data['date']=self.data['date'].dt.strftime('%Y-%m-%d')
-
+        
     def FirstBubbleGraph(self):
         fig = px.scatter(self.data,x="deaths", y="recovered",animation_frame="date", animation_group="country",
             color="continent", 
@@ -38,38 +38,24 @@ class MyWindow(QtWidgets.QMainWindow):
         fig = px.choropleth(self.data, hover_name="country", color="cases",
                            range_color=(0, 5000),
                             locations="countryterritoryCode",color_continuous_scale=px.colors.sequential.Plasma,animation_frame="date", animation_group="country")
-        self.scatter=fig.data[0]
-        self.scatter.on_click(self.update_point)
+
         fileName="MapsGraph.html"
-        fig.write_html("Graphs/"+fileName)
-        self.setupGraph(fileName)
-        self.Graph.show()
-    def update_point(self,trace, points, selector):
-        # c = list(self.scatter.marker.color)
-        # s = list(self.scatter.marker.size)
-        # for i in points.point_inds:
-        #     c[i] = '#bae2be'
-        #     s[i] = 20
-        #     with f.batch_update():
-        #         self.scatter.marker.color = c
-        #         self.scatter.marker.size = s
-        print('hello')
-    def countryGraph(self,data):
-        fig=px.scatter(self.data, x="date", y="cases", animation_frame="date", animation_group="country",
-           size="pop", color="continent", hover_name="country",
-           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
-        fileName="CountryGraph.html"
         fig.write_html("Graphs/"+fileName)
         self.setupGraph(fileName)
         self.Graph.show()
 
     def SortedGraph(self):
-        fig = px.bar(self.data,x='countryterritoryCode',y='cases',hover_name="country", animation_frame="date", animation_group="country")
-        # fig.add_bar(x=self.data['countryterritoryCode'],y=self.data['deaths'])
-      
+        checkValue= str(self.ui.SortComboBox.currentText())
+        if (checkValue=="Cases"):
+            fig = px.bar(self.data,x='countryterritoryCode',y='cases',hover_name="country", animation_frame="date", animation_group="country")
+            fileName="SortedCasesGraph.html"
+        elif (checkValue=="Deaths"):
+            fig = px.bar(self.data,x='countryterritoryCode',y='deaths',hover_name="country", animation_frame="date", animation_group="country")
+            fileName="SortedDeathsGraph.html"
+        else:
+            return
 
         fig.update_layout( xaxis={'categoryorder':'total descending'})
-        fileName="SortedGraph.html"
         fig.write_html("Graphs/"+fileName)
         self.setupGraph(fileName)
         self.Graph.show()
@@ -85,7 +71,6 @@ class MyWindow(QtWidgets.QMainWindow):
         fig.write_html("Graphs/"+fileName)
         self.setupGraph(fileName)
         self.Graph.show()
-
 
 
     def setupGraph(self,fileName):
